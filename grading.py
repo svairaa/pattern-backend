f6a286771596e50f10912aac249fd10f35d23cc7
svairaa <swathishashikumar8@gmail.com>
2026-04-13 01:22:33 +0530

    grading.py

---
+++ grading.py
@@ -0,0 +1,8 @@
+SIZE_CHART = {
+"S": {"bust": 88, "waist": 70, "length": 90},
+"M": {"bust": 92, "waist": 74, "length": 95},
+"L": {"bust": 98, "waist": 80, "length": 100},
+"XL": {"bust": 104, "waist": 86, "length": 105},
+"XXL": {"bust": 110, "waist": 92, "length": 110},
+"XXXL": {"bust": 116, "waist": 98, "length": 115}
+}
