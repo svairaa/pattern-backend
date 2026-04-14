f5a077844c67a221d0dbffb90a95f9ce7172d125
svairaa <swathishashikumar8@gmail.com>
2026-04-14 06:18:02 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "App is working ✅"
 from flask import Flask, render_template, request, redirect, url_for, session, send_file, jsonify
 from werkzeug.utils import secure_filename
 from PIL import Image
